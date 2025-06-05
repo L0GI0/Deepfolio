ff95fe3fdeda3fabd44b7a31d2697f6d5cdc243a
Michal Pabjan <logio@Michals-MacBook-Pro.local>
2025-06-05 12:01:31 +0200

    fix: remove unsuded imports

--- src/containers/skillProgress/skillProgress.js
+++ src/containers/skillProgress/skillProgress.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Progress.scss";
-import {illustration, techStack} from "../../portfolio";
+import {techStack} from "../../portfolio";
 import {Fade} from "react-reveal";
 import Build from "../../assets/lottie/build";
 import DisplayLottie from "../../components/displayLottie/DisplayLottie";
